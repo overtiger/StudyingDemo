83e685580f32dde201e5bda0c6b0badfa388d65f
overtiger <1245772985@qq.com>
2019-09-27 17:37:46 +0800

    visible的位置不对，影响高度计算

--- src/components/notification/notification_use.js
+++ src/components/notification/notification_use.js
@@ -9,6 +9,7 @@ const notificationConstructor = Vue.extend(Component) //创建一个组件
 const instances = [] //保存创建的组件
 let seed = 1 //用来生成id
 
+//每执行一次$notify就执行下面的代码，而上面的仅加载该文件的时候执行一次
 const notify = (options) => {
   if (Vue.prototype.$isServer) return //如果是服务端渲染就直接返回，否则拿不到dom元素
 
@@ -18,7 +19,6 @@ const notify = (options) => {
     propsData: { ...rest },
     data: {
       autoClose: autoClose === undefined ? 3000 : autoClose,
-      visible: true,
     },
   })
 
@@ -26,8 +26,7 @@ const notify = (options) => {
   instance.id = id
   instance.vm = instance.$mount()
   document.body.appendChild(instance.vm.$el)
-  // instance.vm.visible = true    ---->写在这里也可以，写在instance创建的时候也行
-
+  instance.vm.visible = true // ---->写在这里也可以，写在instance创建的时候也行
   //计算离窗口底部的高度，组件之间间隔为16
   let verticalOffset = 0
   instances.forEach((element) => {
